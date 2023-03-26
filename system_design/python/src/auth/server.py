5d6d566a086538cd5e888af2d0fac87733da3a52
arsh0023 <arsharsh2391@gmail.com>
2023-03-26 11:54:56 +0530

    built docker image

--- system_design/python/src/auth/server.py
+++ system_design/python/src/auth/server.py
@@ -35,3 +35,39 @@ def login():
             return createJWT(auth.username, os.environ.get('JWT_SECRET'), True)
     else:
         return "Invalid Credentials", 401
+    
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+            + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz
+        },
+        secret,
+        algorithm="HS256"
+    )
+
+@server.route("/validate",method=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+
+    if not encoded_jwt:
+        return "Missing Credentials", 401
+    
+    encoded_jwt = encoded_jwt.split(" ")
+
+    try:
+        decoded = jwt.decode(
+            encoded_jwt,
+            os.environ.get("JWT_SECRET"),
+            algorithms="HS256"
+        )
+    except:
+        return "Not Authorized", 403
+    
+    return decoded, 200
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0",port=5000)
